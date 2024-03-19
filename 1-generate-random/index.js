daa1522a9ea1cc9d9b120f762cbbff189471be8f
Yuriy <emailworking@ukr.net>
2024-03-19 12:38:00 +0200

    created function rndNum(min,max)

--- 1-generate-random/index.js
+++ 1-generate-random/index.js
@@ -1,1 +1,25 @@
 console.log('RUN !');
+
+function rndNum(min, max) {
+    const CORRECT_ROUND = 0.5;
+    let result = Math.random() * max + 1;
+    result = Math.round(result - min + CORRECT_ROUND);
+    // debugger
+    if (result > max || result < min) {
+        console.log('err', result);
+    }
+    return result;
+}
+
+const min = 1, max = 100;
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
+console.log(rndNum(min, max));
