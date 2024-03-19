8f80d37be048afce7bdbac76e2af65850ade3d43
Yuriy <emailworking@ukr.net>
2024-03-19 13:27:49 +0200

    created function generate sequency num+string with a given range

--- 1-generate-random/index.js
+++ 1-generate-random/index.js
@@ -1,25 +1,76 @@
 console.log('RUN !');
 
 function rndNum(min, max) {
-    const CORRECT_ROUND = 0.5;
-    let result = Math.random() * max + 1;
-    result = Math.round(result - min + CORRECT_ROUND);
+    // проверку при max<min 
+
+
+    //console.log('start - rndNum(min, max)', min, max);
+    let result = Math.random() * (max - min + 1);
+    result = Math.floor(result + min);
     // debugger
     if (result > max || result < min) {
-        console.log('err', result);
+        console.log('err', result, 'min=' + min, 'max=' + max);
     }
+    //console.log('end - rndNum(min, max)');
     return result;
 }
 
 const min = 1, max = 100;
 console.log(rndNum(min, max));
-console.log(rndNum(min, max));
-console.log(rndNum(min, max));
-console.log(rndNum(min, max));
-console.log(rndNum(min, max));
-console.log(rndNum(min, max));
-console.log(rndNum(min, max));
-console.log(rndNum(min, max));
-console.log(rndNum(min, max));
-console.log(rndNum(min, max));
-console.log(rndNum(min, max));
+
+function rndText(n) {
+    console.log('---------------------------');
+    const SEQUENCE_NUM = '0123456789';
+    const SEQUENCE_LITERY_SMALL = 'qwertyuiopasdfghjklzxcvbnm';
+    const SEQUENCE_LITERY_BIG = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+
+    const MAIN_SEQUENCE = SEQUENCE_NUM + SEQUENCE_LITERY_SMALL + SEQUENCE_LITERY_BIG;
+
+    // console.log(MAIN_SEQUENCE);
+
+    const mainSequences = MAIN_SEQUENCE.split('');
+
+    //console.log(mainSequences.length);
+
+    const MAIN_SEQUENCE_LENGTH = mainSequences.length;
+
+    let result = '';
+    let count = 0;
+
+    for (let i = 0; i < n; i++) {
+        count = rndNum(0, MAIN_SEQUENCE_LENGTH - 1);
+        result += mainSequences[count];
+    }
+
+    // console.log(result.length, result)
+
+    return result;
+}
+
+const MIN_2 = 8;
+const MAX_2 = 18;
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
+console.log(rndText(rndNum(MIN_2, MAX_2)));
