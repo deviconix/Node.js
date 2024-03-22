bdb821356889c34638a8bf10f87652a69947c571
Yuriy <emailworking@ukr.net>
2024-03-22 11:26:15 +0200

    backlog

---
+++ 2-read-write/index.js
@@ -0,0 +1,24 @@
+function saveData(pathJSON, folderSave, isRewrite) {
+    // arr=parsingData( pathJSON):array<Object> 
+
+    /* arr.foreach(index=>{
+    
+    })
+    */
+    //cb()
+    //  // getFileName(dataObj<Card>):string
+
+    //  // getView(dataObj<Card>,templateView:string`label ${var} \n`):string
+
+    //  // getSavePath(dataObj<Card>):path
+
+    //  // validate(pathSave<path>):bool
+    //  //  // check(path)
+
+    //  // create SavePath(pathSave<path>):path
+    //  //  // check(pathSave<path>)
+    //  //  //  // if false created(folders)
+
+    // saveFile(pathSave<path>,getFileNameValue,getViewValue) not check 
+    // return foreach cb()
+}
