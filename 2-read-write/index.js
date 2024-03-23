301696fada91057174b09081289aac5d05260774
Yuriy <emailworking@ukr.net>
2024-03-23 16:34:53 +0200

    variant in class app

--- 2-read-write/index.js
+++ 2-read-write/index.js
@@ -12,3 +12,17 @@ import { checkPath } from './libs/path/index.js';
 //const a = checkPath(pathJSON);
 const a = checkPath(pathJSON);
 console.log(a);
+
+const app = new App();
+app.setConfig({ dataTxt, savePath, rewrite=false });
+app.makeCards();
+app.viewList();
+app.viewCard(id);
+app.saveList();
+
+app.saveData();
+
+// commands
+app.run({
+    'gg'
+});
